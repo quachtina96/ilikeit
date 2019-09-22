232401e17ec88707cdd38d44be17f8a8cdb9b4e6
yanni <ycoroneos@gmail.com>
2019-09-21 17:18:55 -0700

    ascii state machine describing behavior

---
+++ arduino/sketch_sep21a/sketch_sep21a.ino
@@ -0,0 +1,30 @@
+void setup() {
+
+  /**
+   *  init the wifi internet connection
+   *  with a hostname so we know where to
+   *  connect to
+   */
+
+}
+
+/**
+ * ilikeit state machine:                                   +------+        new client, refresh token
+ *                                                          |      |  <----------------------------------+
+ *    +-------+      +------------------+                   | IDLE |                                     |
+ *    | RESET | ---->| Run Webserver    |   token obtained  |      |  refresh interval timeout   +------------------+
+ *    +-------+      | to obtain first  | ----------------> |      | --------------------------> | obtain new token |
+ *                   | refresh token    |                   +------+                             | from spotify     |
+ *                   +------------------+                    ^  |                                +------------------+
+ *                                                           |  |  user presses button +-------------------+
+ *                                                           |  +--------------------->| Save current      |
+ *                                                           |                         | track to playlist |
+ *                                                           |                         +-------------------+
+ *                                                           |                                  |
+ *                                                           +----------------------------------+
+ */
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
