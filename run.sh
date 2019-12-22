b6f44e17149f48b1d76d238c4865ffc4a9c6c0ab
yanni <ycoroneos@gmail.com>
2019-12-21 19:31:19 -0800

    remove setting spotify environment vars

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-SPOTIFY_ID="1acbf1f724124569918e0b27034a1a1a" SPOTIFY_SECRET="5fd44baaee234f3f98946c3442ca39c5" GOPATH=$(pwd) go run $1 $2 $3 $4 $5
+GOPATH=$(pwd) go run main.go
